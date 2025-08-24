4b0392087d001ea5cccd8a140375450f2bfb5f79
paulotaraujo <paulo.tap93@gmail.com>
2025-08-24 12:22:53 -0400

    Cria arquivo de dependencia para gerenciar execucao das rotas de forma segura

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import Usuario, db
+from dependencies import pegar_sessao
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def autenticar():
+async def home():
     """
     Esta e a rota padrao de autenticacao do nosso sistema.
     """
     return {"mensagem":"Voce acessou a rota de autenticacao", "autenticado": False}
+
+@auth_router.post("/criar_conta")
+async def crair_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
+
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+
+    if usuario:
+        return{"mensagem":"ja existe um usuario com esse email"}
+    else:
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return{"mensagem":"usuario cadastrado com sucesso!"}
--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class Usuario(Base):
     ativo = Column("ativo", Boolean)
     admin = Column("admin", Boolean, default=False)
 
-    def __init__(self, nome, email, senha, ativo, admin):
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
         self.nome = nome
         self.email = email
         self.senha = senha
